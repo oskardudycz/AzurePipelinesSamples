b7f85232613439378894de662fd5403ae505e921
Oskar Dudycz <oskar.dudycz@gmail.com>
2020-02-22 21:26:26 +0100

    Splitted Build and test into separate jobs

--- Library/Postgres.NET.Tests/SchemaUpdaterTests.cs
+++ Library/Postgres.NET.Tests/SchemaUpdaterTests.cs
@@ -6,14 +6,15 @@ namespace Postgres.NET.Tests
 {
     public class SchemaUpdaterTests
     {
-        public static readonly string ConnectionString = Environment.GetEnvironmentVariable("testing_database")
-                                                         ?? "Host=localhost;Port=5432;Database=postgres;Username=postgres;password=postgres";
+        private static readonly string ConnectionString
+            = Environment.GetEnvironmentVariable("testing_database")
+              ?? "Host=localhost;Port=5432;Database=postgres;Username=postgres;password=postgres";
 
         [Fact]
         public void CreateTable_ShouldSucceed_ForNonEmptyTableName()
         {
             using var schemaUpdater = new SchemaUpdater(new NpgsqlConnection(ConnectionString));
-            
+
             Assert.NotEqual(0, schemaUpdater.CreateTable("testTable"));
         }
     }
